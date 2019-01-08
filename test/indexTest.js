a3e97615c96de892d8100315d3af058d143e845d
Greg Morgan <gregm9999@gmail.com>
2019-01-08 04:39:00 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ const expect = chai.expect;
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1);}`;
 
 describe('index.js', () => {
   describe('currentUser', () => {
