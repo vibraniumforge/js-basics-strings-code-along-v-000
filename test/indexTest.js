d864226196273caa25b15b27a24320ef454abd3c
Greg Morgan <gregm9999@gmail.com>
2019-01-08 04:40:05 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,8 @@
 const expect = chai.expect;
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)};!`;
 
 describe('index.js', () => {
   describe('currentUser', () => {
